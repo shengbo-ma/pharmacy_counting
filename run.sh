7f8f9d64d32a25a83cbbe6d3b74a683bcd2521c6
Shengbo Ma <44483080+shengbo-ma@users.noreply.github.com>
2019-03-03 07:19:06 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
+GRADER_ROOT=$(dirname ${BASH_SOURCE})
+
+PROJECT_PATH=${GRADER_ROOT}/..
+
 python3 ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
